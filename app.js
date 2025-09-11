aacac3557b04522e5f449bc5f4fc69259fddd540
TardelliDias <tardelli.dias@gmail.com>
2025-09-11 10:44:02 -0300

    Conclusão

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let amigos = []; 
 
-function adicionarAmigo() { //criar uma função para adicionar amigos na lista
+function adicionarAmigo() { //Criar uma função para adicionar amigos na lista
     
-    let nome = document.querySelector('input').value; //obter os nomes dos participantes
+    let nome = document.querySelector('input').value; //Obter os nomes dos participantes
 
-    if (nome.trim() !== "") { //condição para ignorar entradas em branco
+    if (nome.trim() !== "") { //Condição para ignorar entradas em branco
                 
-        amigos.push(nome); //adicionar o amigo na lista de participantes 
+        amigos.push(nome); //Adicionar o amigo na lista de participantes 
 
         atualizarAmigos();
         limparCampo();
@@ -16,20 +16,19 @@ function adicionarAmigo() { //criar uma função para adicionar amigos na lista
     }
 
 function limparCampo(){
-    let limpar = document.getElementById('amigo'); //selecionar o campo com o Id amigo do HMTL
-    limpar.value = ''; //limpar o campo de entrada para uma string vazia
+    let limpar = document.getElementById('amigo'); //Selecionar o campo com o Id amigo inserido no HMTL
+    limpar.value = ''; //Limpar o campo de entrada
     limpar.focus();
     }
 
 function atualizarAmigos(){
  
     let novoAmigo = document.getElementById('listaAmigos'); //Obter o elemento HTML para selecionar a lista onde os amigos serão exibidos
-    novoAmigo.innerHTML = ''; //se o HTML já possui algum nome, ele repetirá a listagem. Para evitar, usamos o método .innerHTML
+    novoAmigo.innerHTML = ''; //Se o HTML já possui algum nome, ele repetirá a listagem. Para evitar, usamos o método '.innerHTML'
 
     for (let i = 0; i < amigos.length; i++){
-        
-        novoAmigo = document.createElement('li'); //criar uma tag lista para armazenar o item da lista
-        novoAmigo.textContent = amigos[i]; //alterar o html da li para armaenar o nome do participante
+        novoAmigo = document.createElement('li'); //Criar uma tag do tipo lista para armazenar o item na lista
+        novoAmigo.textContent = amigos[i]; //Alterar o HTML da lista para armazenar o nome do participante
         listaAmigos.appendChild(novoAmigo);
     }
 }
@@ -39,9 +38,8 @@ function sortearAmigo(){
     if (amigos == 0)
         alert('Sem amigos para sortear.');
     else{
-       
-        let aux = Math.floor((Math.random()*amigos.length+1)); //gerar um índice para sorteio
-        let sorteado = amigos[aux-1]; //vincular o indice a um alemento da lista
+        let aux = Math.floor((Math.random()*amigos.length+1)); //Gerar um índice para sorteio
+        let sorteado = amigos[aux-1]; //Vincular o índice a um alemento da lista
         let amigoSorteado = document.getElementById('resultado');
 
         amigoSorteado.textContent = ("O amigo secreto sorteado é: " + sorteado);
